9612b3b97075ccf3bf030dfe9fc3b424172d78b1
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-07-19 22:12:27 +0200

    Switched to createStyleSheet

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,27 +1,6 @@
 import React from 'react'
-import { create } from 'jss'
-import global from 'jss-plugin-global'
-import extend from 'jss-plugin-extend'
-import nested from 'jss-plugin-nested'
-import expand from 'jss-plugin-expand'
-import camel from 'jss-plugin-camel-case'
-import unit from 'jss-plugin-default-unit'
-import functions from 'jss-plugin-rule-value-function'
 import { JssProvider } from 'react-jss'
-
-import fonts from '../fonts'
-
-const jss = create()
-
-jss.use(functions(), global(), extend(), nested(), camel(), unit(), expand())
-
-jss
-  .createStyleSheet({
-    '@global': {
-      ...fonts,
-    },
-  })
-  .attach()
+import jss from './jssConfig'
 
 const injectJss = <P extends Record<string, any>>(
   WrappedComponent: React.ComponentType<P>,
---
+++ src/styles/jssConfig.ts
@@ -0,0 +1,24 @@
+import { create } from 'jss'
+import global from 'jss-plugin-global'
+import extend from 'jss-plugin-extend'
+import nested from 'jss-plugin-nested'
+import expand from 'jss-plugin-expand'
+import camel from 'jss-plugin-camel-case'
+import unit from 'jss-plugin-default-unit'
+import functions from 'jss-plugin-rule-value-function'
+
+import fonts from '../fonts'
+
+const jss = create()
+
+jss.use(functions(), global(), extend(), nested(), camel(), unit(), expand())
+
+jss
+  .createStyleSheet({
+    '@global': {
+      ...fonts,
+    },
+  })
+  .attach()
+
+export default jss
