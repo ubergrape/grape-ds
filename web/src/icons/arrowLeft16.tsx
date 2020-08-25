02707af8ea85744db873f1e36ba688efc6c5f624
Tobias Lins <me@tobi.sh>
2020-08-25 11:03:22 +0200

    Allow currentColor on icons

--- web/src/icons/arrowLeft16.tsx
+++ web/src/icons/arrowLeft16.tsx
@@ -2,11 +2,12 @@ import * as React from 'react'
 
 function SvgArrowLeft16(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width={16} height={16} {...props}>
+    <svg width={16} height={16} fill="none" {...props}>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M8.53 2.47a.75.75 0 010 1.06L4.81 7.25H13a.75.75 0 010 1.5H4.81l3.72 3.72a.75.75 0 11-1.06 1.06l-5-5a.75.75 0 010-1.06l5-5a.75.75 0 011.06 0z"
+        fill="currentColor"
       />
     </svg>
   )
--- web/src/icons/arrowLeft20.tsx
+++ web/src/icons/arrowLeft20.tsx
@@ -2,11 +2,12 @@ import * as React from 'react'
 
 function SvgArrowLeft20(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg width={20} height={20} {...props}>
+    <svg width={20} height={20} fill="none" {...props}>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M10.707 3.293a1 1 0 010 1.414L6.414 9H16a1 1 0 110 2H6.414l4.293 4.293a1 1 0 01-1.414 1.414l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 0z"
+        fill="currentColor"
       />
     </svg>
   )
