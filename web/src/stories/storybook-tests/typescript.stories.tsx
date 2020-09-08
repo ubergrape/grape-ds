39fe30ec77a86d96b921949189ca43c0a546a88d
Tobias Lins <me@tobi.sh>
2020-09-08 16:21:55 +0200

    Remove demo stories

--- web/src/stories/storybook-tests/typescript.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Button from '@storybook/react/dist/demo/Button'
-
-export default { title: 'Typescript' }
-
-export const defaultView = (): JSX.Element => <Button>Button</Button>
