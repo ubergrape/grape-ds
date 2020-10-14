3adfc659da9c201f756203ba79dd888dc4e3f973
Tobias Lins <me@tobi.sh>
2020-10-14 11:31:19 +0200

    Cleanup

---
+++ web/src/components/checkbox/icons.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const CheckmarkIcon = () => (
+  <svg
+    width="12"
+    height="10"
+    viewBox="0 0 12 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1.5 5.5L4.5 8L10.5 1.5" stroke="currentColor" strokeWidth="2" />
+  </svg>
+)
+
+export const IndeterminateIcon = () => (
+  <svg
+    width="12"
+    height="10"
+    viewBox="0 0 12 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1.5 5.5L4.5 8L10.5 1.5" stroke="currentColor" strokeWidth="2" />
+  </svg>
+)
--- web/src/components/checkbox/index.tsx
+++ web/src/components/checkbox/index.tsx
@@ -10,6 +10,7 @@ import { genUid } from '../../utils'
 import { useStyles } from './styles'
 import { useFocusStyle } from '../../styles/global'
 import { Text } from '../typography'
+import { CheckmarkIcon, IndeterminateIcon } from './icons'
 
 export interface Props {
   id?: string
@@ -85,32 +86,8 @@ export const Checkbox: React.FC<CheckboxProps> = props => {
             hasLabel && classes.spacing,
           )}
         >
-          {state.isSelected && (
-            <svg
-              width="12"
-              height="10"
-              viewBox="0 0 12 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1.5 5.5L4.5 8L10.5 1.5"
-                stroke="currentColor"
-                strokeWidth="2"
-              />
-            </svg>
-          )}
-          {indeterminate && !state.isSelected && (
-            <svg
-              width="8"
-              height="2"
-              viewBox="0 0 8 2"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width="8" height="2" fill="currentColor" />
-            </svg>
-          )}
+          {state.isSelected && <CheckmarkIcon />}
+          {indeterminate && !state.isSelected && <IndeterminateIcon />}
         </div>
         {isLabelCheckbox(props) && props.label}
       </div>
