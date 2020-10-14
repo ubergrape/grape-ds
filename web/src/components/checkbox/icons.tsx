418344ed2f4e0db454dac03feccbd7076c5469ca
Tobias Lins <me@tobi.sh>
2020-10-14 13:53:34 +0200

    use correct icon for indeterminate

--- web/src/components/checkbox/icons.tsx
+++ web/src/components/checkbox/icons.tsx
@@ -14,12 +14,12 @@ export const CheckmarkIcon = () => (
 
 export const IndeterminateIcon = () => (
   <svg
-    width="12"
-    height="10"
-    viewBox="0 0 12 10"
+    width="8"
+    height="2"
+    viewBox="0 0 8 2"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M1.5 5.5L4.5 8L10.5 1.5" stroke="currentColor" strokeWidth="2" />
+    <rect width="8" height="2" fill="currentColor" />
   </svg>
 )
