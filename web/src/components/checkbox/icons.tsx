5fafa4a9640583147bbde0cae38476af2c5334e0
Tobias Lins <me@tobi.sh>
2020-10-21 15:27:41 +0200

    Review improvments

--- web/src/components/checkbox/icons.tsx
+++ web/src/components/checkbox/icons.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const CheckmarkIcon = () => (
+export const CheckmarkIcon = (): JSX.Element => (
   <svg
     width="12"
     height="10"
@@ -12,7 +12,7 @@ export const CheckmarkIcon = () => (
   </svg>
 )
 
-export const IndeterminateIcon = () => (
+export const IndeterminateIcon = (): JSX.Element => (
   <svg
     width="8"
     height="2"
