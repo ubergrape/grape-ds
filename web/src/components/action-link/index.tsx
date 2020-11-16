914a72ba0436c430771e348572c85672f3676ff5
Tobias Lins <me@tobi.sh>
2020-11-16 18:54:19 +0100

    Add action link

---
+++ web/src/components/action-link/index.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx'
+import React from 'react'
+import { useFocusStyle } from '../../styles/global'
+
+import { Icon, IconTypes } from '../icon'
+import { Flex } from '../layout'
+import { Text } from '../typography'
+import { useStyles } from './styles'
+
+export type ActionLinkProps = {
+  variant?: 'primary' | 'basic' | 'danger'
+  size?: 'regular' | 'small'
+  href: string
+  target?: '_blank' | '_self'
+  icon?: IconTypes
+  isDisabled?: boolean
+  ariaLabel?: string
+  children: string
+  className?: string
+}
+
+export const ActionLink: React.FC<ActionLinkProps> = props => {
+  const { href, icon, target, ...rest } = props
+  const { isDisabled } = props
+  const style = useStyles(rest)
+  const focus = useFocusStyle({ isInvalid: false, isDisabled })
+  return (
+    <a
+      {...(!isDisabled && { href })}
+      target={target}
+      title={props.children}
+      className={clsx(style.actionLink, focus.onFocus)}
+      {...(isDisabled && { tabIndex: -1 })}
+    >
+      <Flex direction="row" items="center">
+        <div className={style.iconWrapper}>
+          <Icon
+            ariaHidden
+            name={icon || 'search'}
+            size="small"
+            color="primary"
+          />
+        </div>
+        <Text as="span" size="base" className={style.label}>
+          {props.children}
+        </Text>
+      </Flex>
+    </a>
+  )
+}
+
+ActionLink.defaultProps = {
+  variant: 'basic',
+  size: 'regular',
+}
+
+export default ActionLink
---
+++ web/src/stories/action-link/base.stories.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Flex } from '../../components/layout'
+import { ActionLink } from '../../components/action-link'
+
+export default {
+  title: 'Components/ActionLink',
+  component: ActionLink,
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/Mc03kFcaFlcMOA7afjaZmF/Grape-DS-Components-Web?node-id=680%3A2484',
+    },
+  },
+}
+
+const Template = props => {
+  return <ActionLink {...props} />
+}
+export const Base = Template.bind({})
+
+Base.args = {
+  children: 'Go to Google',
+  href: 'https://google.at',
+  target: '_blank',
+}
+
+export const Types = (): JSX.Element => (
+  <Flex gap="2x" direction="column" wrap>
+    <ActionLink href="#" variant="primary">
+      Primary Link
+    </ActionLink>
+    <ActionLink href="#" variant="basic">
+      Basic Link
+    </ActionLink>
+    <ActionLink href="#" variant="danger">
+      Danker Link
+    </ActionLink>
+  </Flex>
+)
+
+export const Sizes = (): JSX.Element => (
+  <Flex gap="2x" direction="column" wrap>
+    <ActionLink href="#" size="regular">
+      Primary Link
+    </ActionLink>
+    <ActionLink href="#" size="small">
+      Basic Link
+    </ActionLink>
+  </Flex>
+)
+
+export const Disabled = (): JSX.Element => (
+  <Flex gap="2x" direction="column" wrap>
+    <ActionLink href="#" variant="primary" isDisabled>
+      Primary Link
+    </ActionLink>
+    <ActionLink href="#" variant="basic" isDisabled>
+      Basic Link
+    </ActionLink>
+    <ActionLink href="#" variant="danger" isDisabled>
+      Danker Link
+    </ActionLink>
+  </Flex>
+)
