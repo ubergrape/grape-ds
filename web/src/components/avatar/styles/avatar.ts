bd815b2d7945e3c1b709ee6bd13f68f5d4424b27
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-12-09 23:08:16 +0100

    Added width and height to avatar wrapper

--- web/src/components/avatar/styles/avatar.ts
+++ web/src/components/avatar/styles/avatar.ts
@@ -8,6 +8,30 @@ import onHover from './onHover'
 export const useAvatarStyle = createUseStyles(
   (theme: typeof tokens): Record<string, JssStyle> => ({
     wrapper: {
+      width: ({ size }) => {
+        switch (size) {
+          case 'x-small':
+            return theme.sizeAvatarXSmall
+          case 'small':
+            return theme.sizeAvatarSmall
+          case 'regular':
+            return theme.sizeAvatarRegular
+          default:
+            return theme.sizeAvatarRegular
+        }
+      },
+      height: ({ size }) => {
+        switch (size) {
+          case 'x-small':
+            return theme.sizeAvatarXSmall
+          case 'small':
+            return theme.sizeAvatarSmall
+          case 'regular':
+            return theme.sizeAvatarRegular
+          default:
+            return theme.sizeAvatarRegular
+        }
+      },
       display: 'flex',
       position: 'relative',
       padding: 0,
