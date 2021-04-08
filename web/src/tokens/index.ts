94f05bae50fd3a8768f5082ccc379b8e803e17df
Stefan Kröner <sk7@users.noreply.github.com>
2021-04-08 14:37:34 +0200

    Add Unicorn theme

--- web/src/tokens/index.ts
+++ web/src/tokens/index.ts
@@ -1,3 +1,19 @@
-import tokens from './tokens.json'
+import grapeTokens from './grape-tokens.json'
+import unicornTokens from './unicorn-tokens.json'
+
+
+const themes = {
+    'grape': grapeTokens,
+    'unicon': unicornTokens,
+}
+
+const theme = (window as any).__THEME__;
+
+console.log("theme", theme)
+
+let tokens = grapeTokens
+if (theme) {
+    tokens = themes[theme]
+}
 
 export default tokens
